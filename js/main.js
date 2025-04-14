8da2da22036cd49a5cf9401034c7343454f6a035
aplaco <lyon108@naver.com>
2025-04-14 17:46:25 +0900

    feat: gsap을 통한 기본 모션 기법

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const wrap = document.querySelector("#wrap");
+const btnMove = document.querySelector(".btnMove");
+const btnReset = document.querySelector(".btnReset");
+
+btnMove.addEventListener("click", () => {
+  gsap.to(".box", {
+    x: 300,
+    y: 200,
+    rotation: 360,
+    duration: 1,
+    backgroundColor: "pink",
+  });
+});
+
+btnReset.addEventListener("click", () => {
+  gsap.to(".box", {
+    x: 0,
+    y: 0,
+    rotation: 0,
+    duration: 0.5,
+    backgroundColor: "aqua",
+  });
+});
